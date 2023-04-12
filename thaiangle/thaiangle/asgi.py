cfbe480692bc935d61abd30d2e14f32368010efa
PNPengy <Parama.Wattanakrai@gmail.com>
2023-04-12 16:49:23 +0700

    Renamed project

--- thaiangle/thaiangle/asgi.py
+++ thaiangle/thaiangle/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for thaiangled project.
+ASGI config for thaiangle project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'thaiangled.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'thaiangle.settings')
 
 application = get_asgi_application()
--- thaiangle/thaiangle/urls.py
+++ thaiangle/thaiangle/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for thaiangled project.
+URL configuration for thaiangle project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
